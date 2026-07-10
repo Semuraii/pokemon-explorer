cebde4b7e8a6c3ada9f870c1a8f28d35d0530c2d
Semuraii <208602227+Semuraii@users.noreply.github.com>
2026-07-10 14:26:14 +0200

    add pokemon mapping

---
+++ src/data/pokemonMap.js
@@ -0,0 +1,7 @@
+export const pokemonMap = {
+    "catering.restaurant": "charmander",
+    "catering.cafe": "eevee",
+    "leisure.park": "bulbasaur",
+    "tourism.hotel": "chansey",
+    "commercial.supermarket": "pikachu"
+};
