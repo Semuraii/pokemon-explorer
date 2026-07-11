23502bfd5088b3bbc68ea42bfcabfc1471475089
Semuraii <208602227+Semuraii@users.noreply.github.com>
2026-07-11 12:15:45 +0200

    pokemonRarity.js file added and pokemon raritys added

---
+++ src/data/pokemonRarity.js
@@ -0,0 +1,83 @@
+export const pokemonRarity = {
+
+    pikachu: "Rare",
+
+    bulbasaur: "Common",
+
+    charmander: "Common",
+
+    squirtle: "Common",
+
+    eevee: "Rare",
+
+    snorlax: "Epic",
+
+    psyduck: "Common",
+
+    vulpix: "Rare",
+
+    growlithe: "Rare",
+
+    oddish: "Common",
+
+    bellsprout: "Common",
+
+    jigglypuff: "Common",
+
+    gastly: "Rare",
+
+    abra: "Rare",
+
+    machop: "Common",
+
+    geodude: "Common",
+
+    ponyta: "Rare",
+
+    magikarp: "Common",
+
+    lapras: "Legendary",
+
+    ditto: "Epic",
+
+    meowth: "Common",
+
+    cubone: "Rare",
+
+    sandshrew: "Common",
+
+    zubat: "Common",
+
+    dratini: "Legendary",
+
+    dragonair: "Legendary",
+
+    pidgey: "Common",
+
+    rattata: "Common",
+
+    spearow: "Common",
+
+    "nidoran-f": "Common",
+
+    "nidoran-m": "Common",
+
+    clefairy: "Rare",
+
+    paras: "Common",
+
+    venonat: "Common",
+
+    diglett: "Common",
+
+    mankey: "Common",
+
+    poliwag: "Common",
+
+    tentacool: "Common",
+
+    slowpoke: "Rare",
+
+    magnemite: "Rare"
+
+};
