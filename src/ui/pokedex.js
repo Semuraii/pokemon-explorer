73964703a13196593d1480d413185a4d7ae76d52
Semuraii <208602227+Semuraii@users.noreply.github.com>
2026-07-13 12:48:57 +0200

    pokemon caught progressbar improved

--- src/ui/pokedex.js
+++ src/ui/pokedex.js
@@ -1,8 +1,5 @@
-let common = 0;
-let rare = 0;
-let legendary = 0;
-
 const caughtPokemon = new Set();
+const TOTAL_POKEMON = 151;
 
 export function addPokemonToPokedex(pokemon) {
 
@@ -44,5 +41,43 @@ pokemonDisplay.innerHTML = `
 
 </div>
 `;
+
+pokemonDisplay.innerHTML = `
+<div class="pokemon-card">
+
+    <img src="${pokemon.sprites.other["official-artwork"].front_default}">
+
+    <h2>${pokemon.name.toUpperCase()}</h2>
+
+    <p class="pokemon-number">
+#${pokemon.id.toString().padStart(3,"0")}
+</p>
+
+    <p>
+        ${pokemon.types
+            .map(type => type.type.name)
+            .join(" • ")}
+    </p>
+
+    <hr>
+
+    <p><strong>Height:</strong> ${pokemon.height / 10} m</p>
+
+    <p><strong>Weight:</strong> ${pokemon.weight / 10} kg</p>
+
+</div>
+`;
+
+const caught = caughtPokemon.size;
+
+document.getElementById("caught-counter").textContent =
+    `Caught Pokémon: ${caught} / ${TOTAL_POKEMON}`;
+
+const percent = (caught / TOTAL_POKEMON) * 100;
+
+document.getElementById("progress-fill").style.width = `${percent}%`;
+
+document.getElementById("progress-percent").textContent =
+    `${Math.round(percent)}%`;
 
 }
