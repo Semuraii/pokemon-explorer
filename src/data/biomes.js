0f7c098902813ceafb8a6996d781a5ba03dfdca7
Semuraii <208602227+Semuraii@users.noreply.github.com>
2026-07-14 11:31:13 +0200

    biomes.js added

---
+++ src/data/biomes.js
@@ -0,0 +1,91 @@
+export const biomes = {
+    forest: [
+        "bulbasaur",
+        "oddish",
+        "bellsprout",
+        "paras",
+        "caterpie",
+        "metapod",
+        "butterfree",
+        "weedle",
+        "kakuna",
+        "beedrill",
+        "pikachu"
+    ],
+
+    water: [
+        "magikarp",
+        "psyduck",
+        "goldeen",
+        "seaking",
+        "poliwag",
+        "poliwhirl",
+        "tentacool",
+        "tentacruel",
+        "horsea",
+        "seadra",
+        "krabby",
+        "kingler",
+        "shellder",
+        "staryu",
+        "slowpoke"
+    ],
+
+    mountain: [
+        "geodude",
+        "graveler",
+        "golem",
+        "onix",
+        "rhyhorn",
+        "rhydon",
+        "sandshrew",
+        "sandslash",
+        "diglett",
+        "dugtrio",
+        "cubone",
+        "marowak",
+        "aerodactyl"
+    ],
+
+    urban: [
+        "meowth",
+        "magnemite",
+        "magneton",
+        "voltorb",
+        "electrode",
+        "grimer",
+        "muk",
+        "koffing",
+        "weezing",
+        "porygon"
+    ],
+
+    residential: [
+        "eevee",
+        "growlithe",
+        "jigglypuff",
+        "clefairy",
+        "abra",
+        "pidgey",
+        "rattata"
+    ],
+
+    fields: [
+        "pidgey",
+        "spearow",
+        "rattata",
+        "nidoran-f",
+        "nidoran-m",
+        "ekans",
+        "mankey",
+        "doduo"
+    ],
+
+    beach: [
+        "krabby",
+        "shellder",
+        "staryu",
+        "tentacool",
+        "horsea"
+    ]
+};
