56434141882d282b8fb8bfd85ee939bb1b53f43e
Semuraii <208602227+Semuraii@users.noreply.github.com>
2026-07-14 11:32:05 +0200

    biomeCategories.js added

---
+++ src/data/biomeCategories.js
@@ -0,0 +1,26 @@
+export const biomeCategories = {
+    // 🌳 Forest
+    "leisure.park": "forest",
+    "natural.forest": "forest",
+
+    // 🌊 Water
+    "natural.water": "water",
+    "natural.beach": "beach",
+
+    // 🪨 Mountains / Rock
+    "natural.rock": "mountain",
+    "natural.cliff": "mountain",
+
+    // 🌾 Fields
+    "natural.grassland": "fields",
+    "landuse.farmland": "fields",
+
+    // 🏙 Urban
+    "commercial.supermarket": "urban",
+    "catering.restaurant": "urban",
+    "catering.cafe": "urban",
+
+    // 🏡 Residential
+    "accommodation.hotel": "residential",
+    "building.residential": "residential"
+};
